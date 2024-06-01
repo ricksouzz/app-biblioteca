8988f1d5a028b2c169cabbef6aec836dc1dac27e
Ricardo Gonçalves <ricardupsg@gmail.com>
2024-05-31 23:20:02 -0300

    validação senha e email

--- src/app/pages/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/pages/cadastro-usuario/cadastro-usuario.component.ts
@@ -29,6 +29,8 @@ import { ToastrService } from 'ngx-toastr';
 export class CadastroUsuarioComponent implements OnInit {
 
   msgs: any;
+  confirmPassword: string = '';
+  passwordsDoNotMatch: boolean = false;
 
   usuario: Usuario = {
     login: '',
@@ -85,10 +87,34 @@ export class CadastroUsuarioComponent implements OnInit {
       this.toastr.error("Preencha um login para o usuário.");
       return false;
     }
+    if (await !this.validarEmail(this.usuario.login)) {
+      this.toastr.error("Preencha um email válido para o login.");
+      return false;
+    }
     if (!this.usuario.password) {
       this.toastr.error('Preencha uma senha para o usuário.');
       return false;
     }
+    if (this.usuario.password.length < 6) {
+      this.toastr.error('A senha deve ter no mínimo 6 caracteres.');
+      return false;
+    }
+    if (this.usuario.password !== this.confirmPassword) {
+      this.passwordsDoNotMatch = true;
+      this.toastr.error('As senhas não coincidem.');
+      return false;
+    }
+    this.passwordsDoNotMatch = false;
     return true;
   }
+
+  clearPasswordLabel(){
+    this.passwordsDoNotMatch = false;
+  }
+
+  validarEmail(email: any) {
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const result = regex.test(email);
+    return result;
+}
 }  
